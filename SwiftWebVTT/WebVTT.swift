67df4c47980b8bbb96ab7077f2b5a88af561c30e
bradchien <bradchien@kktv.me>
2023-02-10 11:48:06 +0800

    add some extension

--- SwiftWebVTT/WebVTT.swift
+++ SwiftWebVTT/WebVTT.swift
@@ -1,13 +1,23 @@
 public struct WebVTT {
-    public struct Cue {
+    public struct Cue: Equatable {
         public let timing: Timing
         public let text: String
+        
+        public init(timing: WebVTT.Timing, text: String) {
+            self.timing = timing
+            self.text = text
+        }
     }
     
     // Native timing in WebVTT. Measured in milliseconds.
-    public struct Timing {
+    public struct Timing: Equatable {
         public let start: Int
         public let end: Int
+        
+        public init(start: Int, end: Int) {
+            self.start = start
+            self.end = end
+        }
     }
     
     public let cues: [Cue]
@@ -26,4 +36,5 @@ public extension WebVTT.Cue {
     var timeStart: TimeInterval { return TimeInterval(timing.start) / 1000 }
     var timeEnd: TimeInterval { return TimeInterval(timing.end) / 1000 }
     var duration: TimeInterval { return TimeInterval(timing.duration) / 1000 }
+    var deviation: TimeInterval { return TimeInterval(timeStart + duration * 0.85) }
 }
